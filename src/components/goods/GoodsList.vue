48dd97f1dfafb06ea1e599e24066ea9b7f0eceb6
chxiaoyu <961041421@qq.com>
2020-03-24 18:38:57 +0800

    完成列表结构

--- src/components/goods/GoodsList.vue
+++ src/components/goods/GoodsList.vue
@@ -1,6 +1,47 @@
 <template>
-  <div>
-    <h3>商品列表</h3>
+  <div class="goods-list">
+    <div class="goods-item">
+      <img src alt />
+      <h3 class="title">小米（Mi）小米Note 16G双网通版</h3>
+      <div class="info">
+        <p class="price">
+          <span class="now">￥899</span>
+          <span class="old">￥999</span>
+        </p>
+        <p class="sell">
+          <span>热卖中</span>
+          <span>剩余60件</span>
+        </p>
+      </div>
+    </div>
+    <div class="goods-item">
+      <img src alt />
+      <h3 class="title">小米（Mi）小米Note 16G双网通版</h3>
+      <div class="info">
+        <p class="price">
+          <span class="now">￥899</span>
+          <span class="old">￥999</span>
+        </p>
+        <p class="sell">
+          <span>热卖中</span>
+          <span>剩余60件</span>
+        </p>
+      </div>
+    </div>
+    <div class="goods-item">
+      <img src alt />
+      <h3 class="title">小米（Mi）小米Note 16G双网通版</h3>
+      <div class="info">
+        <p class="price">
+          <span class="now">￥899</span>
+          <span class="old">￥999</span>
+        </p>
+        <p class="sell">
+          <span>热卖中</span>
+          <span>剩余60件</span>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +49,54 @@
 export default {};
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.goods-list {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 7px;
+  justify-content: space-between;
+
+  .goods-item {
+    width: 49%;
+    border: 1px solid #ccc;
+    box-shadow: 0 0 7px #ccc;
+    margin: 3px 0;
+    padding: 2px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 293px;
+
+    img {
+      width: 100%;
+    }
+
+    .title {
+      font-size: 14px;
+    }
+
+    .info {
+      background-color: #eeeeee;
+      p {
+        margin: 0;
+        padding: 5px;
+      }
+      .price {
+        .now {
+          color: red;
+          font-size: 16px;
+          font-weight: bold;
+        }
+        .old {
+          text-decoration: line-through;
+        }
+      }
+      .sell {
+        display: flex;
+        justify-content: space-between;
+        font-size: 13px;
+      }
+    }
+  }
+}
+</style>
